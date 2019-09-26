7ff16686b2b3ef2b966dccb92f24e10e6d36a248
ASUS <ASUS@DESKTOP-6TMOBF9>
2019-09-26 18:39:49 +0700

    commit

---
+++ src/pbo2/pert1/pkg10118046/latihan1/biodata/PBO2PERT110118046Latihan1Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pert1.pkg10118046.latihan1.biodata;
+
+/**
+ *
+ * @author
+ *  Nama              : M. Hilmy Naufal
+ *  Kelas             : IF2
+ *  NIM               : 10118046
+ *  Deskripsi Program : menampilkan biodata mahasiswa ROCKSTAR
+ */
+public class PBO2PERT110118046Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "-----------------------");
+        System.out.println("Nama\t\t: M. Hilmy Naufal");
+        System.out.println("ALAMAT\t\t: Soreang Residence");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 05 Mei 1999");
+        System.out.println("-----------------------");
+        System.out.println("HOBI\t\t: Gaming & Sleeping");
+        System.out.println("MAKES\t\t: Indomie");
+        System.out.println("MIKES\t\t: Sprite");
+    }
+    
+}
